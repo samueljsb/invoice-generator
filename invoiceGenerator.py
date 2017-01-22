cba4cd5e03edd9bd408a437585db1a99d83a6f8e
Sam Searles-Bryant <sam@samueljsb.co.uk>
2017-01-22 17:59:03 +0000

    Clean up
    Tidied up some formatting and removed unnecessary vars.
    Also added options to set location of config, customers files.

--- invoiceGenerator.py
+++ invoiceGenerator.py
@@ -7,12 +7,10 @@ Invoice Generator
 Author: Samuel Searles-Bryant
 Date created: 2016-02-22
 
-Last updated: 2016-10-07
+Last updated: 2017-01-22
 
 N.B.    This program requires pdflatex.
 '''
-
-VERSION = "v0.3.5"
 
 # Import modules
 import json, csv # for opening/saving files and data
@@ -28,10 +26,15 @@ logging.disable(logging.INFO) # disable all log messages for DEBUG and INFO
 #logging.disable(logging.CRITICAL) # disable *all* log messages
 
 # Options
-CURRENCY = "GBP" # N.B. this does not yet affect functionality
-titleWidth = 50 # width of title bars
+titleSplash = '''
+%------------------------------------------------%
+                Invoice Generator
+%------------------------------------------------%
+'''
 pathToSave = os.path.expanduser('~/Dropbox/Invoices/')# set destination directory of generated invoices
 pathToCSV = os.path.expanduser('~/Desktop/invoiceData.csv')
+pathToCustomers = os.path.expanduser('~/Dropbox/Invoices/customers.json')
+pathToConfig = os.path.expanduser('~/Dropbox/Invoices/config.json')
 
 # Check we're using Python3
 try:
@@ -90,7 +93,7 @@ def generateInvoice(invoice):
 
     # Write TEMPconfig.tex
     logging.debug("Opening config")
-    configFile = open('config.json','r')
+    configFile = open(pathToConfig,'r')
     configData = json.load(configFile)
     userInfo = (configData['userName'],configData['userAddress'],configData['userPhoneNumber'],configData['userEmail'],configData['accountNumber'],configData['sortCodeFormatted'])
     configInfo = r"\newcommand\myName{{{userName}}}\newcommand\myAddress{{{userAddress}}}\newcommand\myPhoneNumber{{{userPhoneNumber}}}\newcommand\myEmail{{{userEmail}}}\newcommand\accountNumber{{{accountNumber}}}\newcommand\sortCode{{{sortCodeFormatted}}}".format(**configData)
@@ -130,6 +133,83 @@ def generateInvoice(invoice):
 
 
 ##### MENUS #####
+
+def mainMenu():
+    ## Main menu
+
+    while True:
+        printUnderline("Main Menu",char="=",width=15)
+        print("""1: New invoice
+        \r2: New customer
+        \r3: Edit existing invoice
+        \r4: Run config util
+        \rexit: Save and exit""")
+
+        while True:
+            menuChoice = input(">> ")
+            if menuChoice == '1': ## New invoice
+
+                if customerAccounts == {}:
+                    print( "There are no customers registered. Please register a customer before trying to generate an invoice")
+                    break
+
+                newInvoice = Invoice(customerAccounts)
+                newInvoiceMenu(newInvoice)
+
+                mainMenu()
+
+            elif menuChoice == '2': ## New customer
+                printUnderline( "\nNew customer" )
+                try:
+                    # Ask for customer name
+                    inputName = tryInput("What is the new customer's name? ")
+
+                    # Ask for customer address
+                    inputAddress = addressInput()
+
+                    # Ask for account code
+                    inputAccountCode = tryInput("Please enter an account code for this customer: ")
+
+                    # Create new customer account
+                    customerAccounts[inputAccountCode] = CustomerAccount(inputAccountCode,inputName,inputAddress,0)
+                    print( "Successfully created new customer account: {}".format(inputAccountCode) )
+                except NoInputError:
+                    break
+
+                mainMenu()
+
+            elif menuChoice == '3': ## Edit existing invoice
+
+                inDevelopment('Edit existing invoice',error=True)
+
+                mainMenu()
+
+            elif menuChoice == '4': ## Run config util
+
+                configUtil()
+
+                mainMenu()
+
+            elif menuChoice == '0' or menuChoice.lower() == 'exit': ## Save and exit
+                print( "\nSaving data..." )
+
+                # Create dictionary for JSON file
+                dataToSave = {}
+                for account in customerAccounts:
+                    dataToSave[customerAccounts[account].getAccountName()] = customerAccounts[account].JSONdump()
+
+                # Save data to JSON file
+                jsonFile = open(pathToCustomers, 'w+')
+                jsonFile.write(json.dumps(dataToSave))
+                jsonFile.close()
+
+                print( "Data saved!" )
+                print( "Goodbye!\n" )
+                exit() # exit program
+
+            else:
+                    print( "That is not a valid choice. Please try again:" )
+
 
 def newInvoiceMenu(invoice):
     '''
@@ -255,7 +335,7 @@ def configUtil():
     configData['sortCodeFormatted'] = sortCodeFormatted
 
     # Save data to JSON file
-    jsonFile = open("config.json", 'w+')
+    jsonFile = open(pathToConfig, 'w+')
     jsonFile.write(json.dumps(configData))
     jsonFile.close()
 
@@ -269,19 +349,16 @@ if __name__ == "__main__":
     os.chdir(os.path.dirname(__file__)) # cd to the location of this python file (and associated data files)
     os.system('clear')
 
-    print( "%"+"-"*(titleWidth-2)+"%" )
-    print( "Invoice Generator".center(titleWidth) )
-    print( "%"+"-"*(titleWidth-2)+"%" )
-    print( "{}{}".format(VERSION.ljust(titleWidth-len(CURRENCY)), CURRENCY) )
+    print(titleSplash)
 
     assert(not os.path.exists('TEMPfiles/')) # make sure there are no left over TEMPfiles
 
     ## Congifuration
-    if not os.path.exists('config.json'):
+    if not os.path.exists(pathToConfig):
         print( "No configuration information found. Running configUtil..." )
         configUtil()
     else:
-        jsonFile = open("config.json",'r')
+        jsonFile = open(pathToConfig,'r')
         configData = json.load(jsonFile)
         jsonFile.close()
         print( "Welcome, {userName}!".format(**configData) )
@@ -291,12 +368,12 @@ if __name__ == "__main__":
     print( "\nLoading cutomer data..." )
     customerAccounts = {} # dictionary of customer accounts
 
-    if not os.path.exists('customers.json'): # if no customer data, create a file
+    if not os.path.exists(pathToCustomers): # if no customer data, create a file
         print( "There is no customer data. A new file will be created." )
-        newFile = open("customers.json",'w')
+        newFile = open(pathToCustomers,'w')
         newFile.close()
     else:
-        jsonFile = open("customers.json",'r')
+        jsonFile = open(pathToCustomers,'r')
         customerData = json.load(jsonFile)
         jsonFile.close()
         for account in customerData: # build dictionary of CustomerAccount objects. NB: keys are lowercase!
@@ -304,77 +381,4 @@ if __name__ == "__main__":
 
         print( "Customer data loaded successfully!" )
 
-    ## Main menu
-
-    while True:
-        printUnderline("Main Menu",char="=",width=15)
-        print("""1: New invoice
-        \r2: New customer
-        \r3: Edit existing invoice
-        \r4: Run config util
-        \rexit: Save and exit""")
-
-        while True:
-            menuChoice = input(">> ")
-            if menuChoice == '1': ## New invoice
-
-                if customerAccounts == {}:
-                    print( "There are no customers registered. Please register a customer before trying to generate an invoice")
-                    break
-
-                newInvoice = Invoice(customerAccounts)
-                newInvoiceMenu(newInvoice)
-
-                break # return to main menu
-
-            elif menuChoice == '2': ## New customer
-                printUnderline( "\nNew customer" )
-                try:
-                    # Ask for customer name
-                    inputName = tryInput("What is the new customer's name? ")
-
-                    # Ask for customer address
-                    inputAddress = addressInput()
-
-                    # Ask for account code
-                    inputAccountCode = tryInput("Please enter an account code for this customer: ")
-
-                    # Create new customer account
-                    customerAccounts[inputAccountCode] = CustomerAccount(inputAccountCode,inputName,inputAddress,0)
-                    print( "Successfully created new customer account: {}".format(inputAccountCode) )
-                except NoInputError:
-                    break
-
-                break # return to main menu
-
-            elif menuChoice == '3': ## Edit existing invoice
-
-                inDevelopment('Edit existing invoice',error=True)
-
-                break # return to main menu
-
-            elif menuChoice == '4': ## Run config util
-
-                configUtil()
-
-                break # return to main menu
-
-            elif menuChoice == '0' or menuChoice.lower() == 'exit': ## Save and exit
-                print( "\nSaving data..." )
-
-                # Create dictionary for JSON file
-                dataToSave = {}
-                for account in customerAccounts:
-                    dataToSave[customerAccounts[account].getAccountName()] = customerAccounts[account].JSONdump()
-
-                # Save data to JSON file
-                jsonFile = open("customers.json", 'w+')
-                jsonFile.write(json.dumps(dataToSave))
-                jsonFile.close()
-
-                print( "Data saved!" )
-                print( "Goodbye!\n" )
-                exit() # exit program
-
-            else:
-                    print( "That is not a valid choice. Please try again:" )
+    mainMenu()
